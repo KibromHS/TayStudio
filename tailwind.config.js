b5ae081aa946f8d2b36af123726fc3daee4135aa
KibromHS <kibromhs81@gmail.com>
2023-04-06 20:20:07 +0300

    two view pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./dist/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero': "linear-gradient(to bottom, rgba(28, 33, 87, 0.5), rgba(28, 33, 87, 0.5)), url('./img/home.png');",
+      },
+      height: {
+        'full': '100vh'
+      },
+      fontFamily: {
+        'kaushan': 'Kaushan Script',
+        'litsans': 'LitSans'
+      },
+      width: {
+        'kb': '300rem'
+      }
+    },
   },
   plugins: [],
 }
